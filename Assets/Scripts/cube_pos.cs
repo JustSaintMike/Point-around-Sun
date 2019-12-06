e462db12cc75bf90be71ee1c370b36a62ced96cd
Михаил Кузнецов <kuznetsOFFmikhail@outlook.com>
2019-12-06 11:27:03 +0500

    Движение 2 и 3 тел
    Добаленны расчтены и анимация для 2х и 3х тел

--- Assets/Scripts/cube_pos.cs
+++ Assets/Scripts/cube_pos.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class cube_pos : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
+	Rigidbody rb;
+	public Rigidbody rb1;
+	public Vector3 r;
+	public Vector3 R;
+	public Vector3 V;
+	float g = 6.67408e-2F;
+	public float m;
+	public float M;
+
+
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.position= r;
+		rb.velocity = V;
+
+		Debug.Log (rb.position);
+	}
+
+	void Update ()
+	{
 		
+		r = rb.position-rb1.position;
+		R = rb.position;
+		float l = -g * m / Mathf.Pow (r.magnitude, 3);
+		float L = -g * M / Mathf.Pow (R.magnitude, 3);
+		rb.AddForce (l*r+L*R);
+		Debug.Log (l*r);
+
 	}
 }
